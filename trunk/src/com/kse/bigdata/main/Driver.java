d9e258b577017d7936eeed772a84cb9bde366148
osehyum@gmail.com <osehyum@gmail.com@16fc2e54-82d1-8f2b-403c-17011e477e14>
2014-12-08 05:17:21 +0000

    Cleaning Code.

--- trunk/src/com/kse/bigdata/main/Driver.java
+++ trunk/src/com/kse/bigdata/main/Driver.java
@@ -17,7 +17,6 @@ package com.kse.bigdata.main;
 
 import com.kse.bigdata.entity.Sequence;
 import com.kse.bigdata.file.SequenceSampler;
-import org.apache.commons.math3.analysis.function.Log;
 import org.apache.commons.math3.ml.distance.EuclideanDistance;
 import org.apache.commons.math3.stat.descriptive.moment.Mean;
 import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
@@ -40,7 +39,10 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.*;
 import java.math.BigDecimal;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 /**
  * Created by user on 2014-12-02.
@@ -50,7 +52,7 @@ public class Driver {
 
 //    public static class Combiner extends Reducer<NullWritable, Text, NullWritable, Text>{
 //
-//        private SortedSet<Sequence> sequences = new TreeSet<>();
+//        private SortedSet<Sequence> neighbors = new TreeSet<>();
 //        private final int BUFFER_SIZE = 200;
 //
 //        @Override
@@ -59,47 +61,44 @@ public class Driver {
 //
 //            for(Text value : values) {
 //                Sequence newSeq = new Sequence(value.toString());
-//                addWordToSortedSet(newSeq);
+//                addSeqToNeighbors(newSeq);
 //
-//                if(newSeq.getEuclideanDistance() <= sequences.last().getEuclideanDistance())
+//                if(newSeq.getEuclideanDistance() <= neighbors.last().getEuclideanDistance())
 //                    context.write(NullWritable.get(), value);
 //
 //            }
 //        }
 //
-//        private void addWordToSortedSet(Sequence newSeq){
-//            sequences.add(newSeq);
+//        private void addSeqToNeighbors(Sequence newSeq){
+//            neighbors.add(newSeq);
 //
-//            if(sequences.size() > BUFFER_SIZE)
-//                sequences.remove(sequences.last());
+//            if(neighbors.size() > BUFFER_SIZE)
+//                neighbors.remove(neighbors.last());
 //        }
 //    }
 
     public static class Map extends Mapper<LongWritable, Text, NullWritable, Text> {
-        public static final String NUMBER_OF_NEAREAST_NEIGHBOR  = "nnn";
+        public static final String NUMBER_OF_NEAREST_NEIGHBOR   = "nnn";
         public static final String NORMALIZATION                = "normalize";
         public static final String INPUT_SEQUENCE               = "inputSeq";
 
         private boolean normalization         = false;
         private int NUMBER_OF_NEIGHBOR        = 100;
-
+        private int previousSourceFileId      = -1;
         private Sequence userInputSequence;
 
-        private Text result = new Text();
+        private final Text                result     = new Text();
+        private LinkedList<Double>        buffer     = new LinkedList<>();
+        private final SortedSet<Sequence> neighbors  = new TreeSet<>();
 
-        private LinkedList<Double> temp;
-        private SortedSet<Sequence> sequences = new TreeSet<>();
-
-        private HashMap<Integer, LinkedList<Double>> tempSequence = new HashMap<>();
-
-        private StandardDeviation standardDeviation = new StandardDeviation();
-        private Mean              mean              = new Mean();
-        private EuclideanDistance euclideanDistance = new EuclideanDistance();
+        private final StandardDeviation standardDeviation = new StandardDeviation();
+        private final Mean              mean              = new Mean();
+        private final EuclideanDistance euclideanDistance = new EuclideanDistance();
 
         @Override
         public void setup(Context context) throws IOException{
             userInputSequence      = new Sequence(context.getConfiguration().get(INPUT_SEQUENCE, ""));
-            NUMBER_OF_NEIGHBOR = context.getConfiguration().getInt(NUMBER_OF_NEAREAST_NEIGHBOR, 100);
+            NUMBER_OF_NEIGHBOR     = context.getConfiguration().getInt(NUMBER_OF_NEAREST_NEIGHBOR, 100);
             normalization          = context.getConfiguration().getBoolean(NORMALIZATION, false);
         }
 
@@ -107,38 +106,35 @@ public class Driver {
         @Override
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
             String[] tokens = value.toString().split(",");
-            double powerGeneration = Double.parseDouble(tokens[0]);
-            int sourceFileId = Integer.parseInt(tokens[1]);
+            Double windSpeed = Double.valueOf(tokens[0]);
+            int currentSourceFileId = Integer.parseInt(tokens[1]);
 
-            if(tempSequence.containsKey(sourceFileId)) {
-                tempSequence.get(sourceFileId).add(powerGeneration);
-
-            } else {
-                tempSequence.put(sourceFileId, new LinkedList<Double>());
-
+            if(currentSourceFileId != previousSourceFileId){
+                buffer.clear();
+                previousSourceFileId = currentSourceFileId;
             }
 
-            temp = tempSequence.get(sourceFileId);
+            buffer.add(windSpeed);
 
-            if(temp.size() == Sequence.SIZE_OF_SEQUENCE){
-                Sequence newSeq = new Sequence(temp);
+            if(buffer.size() == Sequence.SIZE_OF_SEQUENCE){
+                Sequence newSeq = new Sequence(buffer);
                 newSeq.setEuclideanDistance(calculateEuclideanDistance(newSeq, userInputSequence));
-                addWordToSortedSet(newSeq);
-                temp.removeFirst();
+                addSeqToNeighbors(newSeq);
+                buffer.removeFirst();
             }
 
         }
 
-        private void addWordToSortedSet(Sequence newSeq){
-            sequences.add(newSeq);
+        private void addSeqToNeighbors(Sequence newSeq){
+            neighbors.add(newSeq);
 
-            if(sequences.size() > NUMBER_OF_NEIGHBOR)
-                sequences.remove(sequences.last());
+            if(neighbors.size() > NUMBER_OF_NEIGHBOR)
+                neighbors.remove(neighbors.last());
         }
 
         @Override
         public void cleanup(Context context) throws InterruptedException, IOException{
-            for(Sequence seq : sequences) {
+            for(Sequence seq : neighbors) {
                 result.set(seq.toString());
                 context.write(NullWritable.get(), result);
             }
@@ -174,24 +170,24 @@ public class Driver {
         public static final String NUMBER_OF_NEAREAST_NEIGHBOR = "nnn";
         public static final String INPUT_SEQUENCE              = "inputSeq";
 
-        //Parameters for rounding method.
-        private final int DECIMAL_SCALE   = 3;
-        private final int ROUNDING_METHOD = BigDecimal.ROUND_HALF_UP;
-
         private int     NUMBER_OF_NEIGHBOR = 0;
         private boolean USE_MEDIAN         = false;
 
+        //Parameters for rounding method.
+        private final int    DECIMAL_SCALE   = 3;
+        private final int    ROUNDING_METHOD = BigDecimal.ROUND_HALF_UP;
+        private final String WHITE_SPACE     = " ";
+
         // Variables for reducing the cost of creating instance.
-        private Text tempKey = new Text();
-        private Text tempValue = new Text();
+        private final Text predictedSeq   = new Text();
+        private final Text error          = new Text();
 
         private Sequence userInputSequence;
+        private final SortedSet<Sequence> neighbors = new TreeSet<>();
 
-        private SortedSet<Sequence> sequences = new TreeSet<>();
-
-        private Mean mean     = new Mean();
-        private Log  log      = new Log();
-        private Median median = new Median();
+        private final Mean   mean     = new Mean();
+        private final Median median   = new Median();
+//        private Log  log      = new Log();
 
         @Override
         public void setup(Context context) throws IOException{
@@ -206,8 +202,15 @@ public class Driver {
                 throws IOException, InterruptedException {
             for(Text value : values) {
                 Sequence newSeq = new Sequence(value.toString());
-                addWordToSortedSet(newSeq);
+                addSeqToNeighbors(newSeq);
             }
+        }
+
+        private void addSeqToNeighbors(Sequence newSeq){
+            neighbors.add(newSeq);
+
+            if(neighbors.size() > NUMBER_OF_NEIGHBOR)
+                neighbors.remove(neighbors.last());
         }
 
         @Override
@@ -221,15 +224,15 @@ public class Driver {
                 if(index == (predictedValues.length - 1))
                     break;
 
-                stringBuilder.append("-");
+                stringBuilder.append(Sequence.DELIMITER);
             }
 
             double MER = calculateMeanErrorRate(predictedValues, userInputSequence.getTail());
             double MAE = calculateMeanAbsoluteError(predictedValues, userInputSequence.getTail());
 
-            tempKey.set(stringBuilder.toString());
-            tempValue.set(String.valueOf(MER) + " " + String.valueOf(MAE));
-            context.write(tempKey, tempValue);
+            predictedSeq.set(stringBuilder.toString());
+            error.set(String.valueOf(MER) + WHITE_SPACE + String.valueOf(MAE));
+            context.write(predictedSeq, error);
         }
 
         private double[] predictSequence(){
@@ -241,13 +244,13 @@ public class Driver {
             double weight;
 
             int counter = 0;
-            for(Sequence seq : this.sequences) {
+            for(Sequence seq : this.neighbors) {
                 tailOfSeq = seq.getTail();
 
                 if (USE_MEDIAN) {
                     for (int index = 0; index < Sequence.SIZE_OF_TAIL_SEQ; index++) {
                         if(tails[index] == null)
-                            tails[index] = new double[this.sequences.size()];
+                            tails[index] = new double[this.neighbors.size()];
 
                         tails[index][counter] = tailOfSeq[index];
                     }
@@ -255,7 +258,7 @@ public class Driver {
                     counter++;
 
                 } else {
-                    weight = calculateWeight(seq);
+                    weight = 1.0d;//calculateWeight();
                     sumOfWeights += weight;
 
                     for (int index = 0; index < Sequence.SIZE_OF_TAIL_SEQ; index++) {
@@ -277,22 +280,6 @@ public class Driver {
             return predictionResult;
         }
 
-        private void addWordToSortedSet(Sequence newSeq){
-            sequences.add(newSeq);
-
-            if(sequences.size() > NUMBER_OF_NEIGHBOR)
-                sequences.remove(sequences.last());
-        }
-
-        private double calculateWeight(Sequence seq){
-            double eucDist = seq.getEuclideanDistance();
-
-            if(eucDist == 0)
-                eucDist = 0.0001;
-
-            return log.value(1.0d/eucDist);
-        }
-
         private double calculateMeanErrorRate(double[] predictedSeq, double[] actualSeq){
             double meanOfActualSeq = mean.evaluate(actualSeq);
             double error = 0.0d;
@@ -311,6 +298,15 @@ public class Driver {
 
             return Precision.round((error / Sequence.SIZE_OF_TAIL_SEQ), DECIMAL_SCALE, ROUNDING_METHOD);
         }
+
+        //        private double calculateWeight(){
+//            double eucDist = seq.getEuclideanDistance();
+
+//            if(eucDist == 0)
+//                eucDist = 0.0001;
+
+//            return 1.0d;//log.value(1.0d/eucDist);
+//        }
 
     }
 
@@ -391,6 +387,12 @@ public class Driver {
         SequenceSampler sampler = new SequenceSampler(testPath, sampleSize);
         LinkedList<Sequence> testSequences = sampler.getRandomSample();
 
+//        Test Sequence
+//        String testSeqString = "13.591-13.674-13.778-13.892-13.958-14.049-14.153-14.185-14.169-14.092-13.905-13.702-13.438-13.187-13.0-12.914-12.868-12.766-12.62-12.433-12.279-12.142-12.063-12.025-100";
+//        Sequence testSeq = new Sequence(testSeqString);
+//        LinkedList<Sequence> testSequences = new LinkedList<>();
+//        testSequences.add(testSeq);
+
         for (Sequence seq : testSequences) {
 
             /*
@@ -413,11 +415,11 @@ public class Driver {
 //          Current Implementation is not helpful to Job.
 //          job.setCombinerClass(Combiner.class);
 
-            //Set 1 for number of reduce task for keeping 100 most sequences in sorted set.
+            //Set 1 for number of reduce task for keeping 100 most neighbors in sorted set.
+            job.setNumReduceTasks(1);
             job.setReducerClass(Reduce.class);
             job.setOutputKeyClass(Text.class);
             job.setOutputValueClass(Text.class);
-            job.setNumReduceTasks(1);
 
             job.setInputFormatClass(TextInputFormat.class);
             job.setOutputFormatClass(TextOutputFormat.class);
@@ -437,7 +439,7 @@ public class Driver {
 
                 String line;
                 while ((line = fileReader.readLine()) != null) {
-                    results.add(seq.getTailString() + " " + line);
+                    results.add(seq.getSeqString() + " " + line);
                 }
 
                 fileReader.close();
@@ -453,7 +455,7 @@ public class Driver {
         /*
          * if all jobs finish, store results of jobs to output/result.txt file.
          */
-        String finalOutputPath = "output/result.txt";
+        String finalOutputPath = "output/result.csv";
         try (FileSystem hdfs = FileSystem.get(new Configuration())) {
 
             Path file = new Path(finalOutputPath);
@@ -464,41 +466,18 @@ public class Driver {
             OutputStream os = hdfs.create(file);
             PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(os, "UTF-8"));
 
-            double totalMER = 0.0d;
-            double totalMAE = 0.0d;
-
-            int MERCounter = 0;
+            //CSV File Header
+            printWriter.println("Actual,Predicted,MER,MAE");
+            printWriter.flush();
 
             for (String result : results) {
                 String[] tokens = result.split("\\s+");
 
-                double MER = Double.valueOf(tokens[2]);
-                if(!Double.isInfinite(MER)) {
-                    totalMER += MER;
-                    MERCounter++;
-                }
-
-                totalMAE += Double.valueOf(tokens[3]);
-
-                String actualSeq = "A:" + tokens[0];
-                String predictedSeq = "P:" + tokens[1];
-                String errorOutput = " [ MER : " + tokens[2] + " MAE : " + tokens[3] + " ]";
-
-                printWriter.println(actualSeq);
-                printWriter.println(predictedSeq);
-                printWriter.println(errorOutput);
+                printWriter.println(tokens[0] + "," + tokens[1] + "," + tokens[2] +"," + tokens[3]);
                 printWriter.flush();
             }
 
-            String errorOutputString = "[ AVG.MER : " +
-                    Precision.round(totalMER / MERCounter, 2, BigDecimal.ROUND_HALF_UP) +
-                    "  AVG.MAE : " + Precision.round(totalMAE / results.size(), 2, BigDecimal.ROUND_HALF_UP) + " ]";
-
-            printWriter.println(errorOutputString);
-            printWriter.flush();
-
             printWriter.close();
-
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
